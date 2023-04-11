0c531bb136e5695119564b7ad0603b196d19f193
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-04-11 17:17:18 +0530

    updated

--- test.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const message = process.env.text_msg;
-
-  const user = "lahiru";
-  const formattedMessage = message.replace('%user%', user);
-  console.log(formattedMessage, { parse_mode: 'HTML' });
